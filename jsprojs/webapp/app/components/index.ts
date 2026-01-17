046ebbd5eaa57789b0945be1c6eae92ef3f4d3dc
vrnvrn <vrneth@proton.me>
2026-01-18 00:28:44 +0800

    Add components index and integrate design tokens

---
+++ jsprojs/webapp/app/components/index.ts
@@ -0,0 +1,3 @@
+export { default as WalletConnection } from './WalletConnection';
+export { default as ChatInterface } from './ChatInterface';
+export { default as MemoryVisualization } from './MemoryVisualization';
